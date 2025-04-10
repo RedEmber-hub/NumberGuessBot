51615944b1362923c23de07ce77d73d2e4efd659
RedEmber-hub <redember38@gmail.com>
2025-04-10 22:13:52 +0300

    дописан функционал игрового бота

--- js/scipt.js
+++ js/scipt.js
@@ -1,8 +1,10 @@
 let numberInput;
 let title;
 let secretNumber;
+let guessCount;
+let playAgain; //кол-во попыток
 
-// функция для проверки, является ли введенное значение числом
+// функция для проверки, является ли значение числом
 const isNumber = (num) => {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
@@ -25,40 +27,73 @@ const validateInput = (input) => {
 }
 
 //функция на проверку нажатия кнопки "отмена" в prompt 
-const checkCancel = (input) => {
-    return input === null; //вернёт true
-}
+const checkCancel = (input) => input === null; //вернёт true
 
 //функция генерации рандомного числа в диапазоне
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-//функция угадывания числа
-const guessNumber = () => {
-    do {
-        numberInput = prompt("Введи число от 1 до 100");
+//ф-ция с попытками угадывания числа
+const updateGuessCount = () => {
+    guessCount--;
 
-        if (checkCancel(numberInput)) {
-            alert("Игра окончена");
-            return;
-        }
-
-        if (!validateInput(numberInput)) continue;
-
-        numberInput = parseFloat(numberInput);
-
-        if (numberInput === secretNumber) {
-            alert("Ты угадал!");
-            break;
+    if (guessCount <= 0) {
+        playAgain = confirm('Попытки закончились, хотите сыграть еще?');
+        if (playAgain) {
+            startGame();
         } else {
-            alert("попробуй еще раз!");
+            alert("Ты отказался от игры. Может быть, в следующий раз!");
         }
+        return true; // игра останавливается
+    }
+    return false; // игра продолжается
+}
 
-    } while (true);
+//ф-ция проверки угадывания числа
+const checkGuess = (numberInput) => {
+    if (numberInput === secretNumber) {
+        playAgain = confirm("Поздравляю, ты угадал!!! Хотел бы сыграть еще?");
+        if (playAgain) {
+            startGame();
+            return true;
+        }
+        return true; // игра остановется 
+    }
+
+    const gameEnded = updateGuessCount(); // обновляем кол-во попыток
+
+    if (!gameEnded) { // если игра продолжается
+        if (numberInput > secretNumber) {
+            alert(`Загаданное число меньше, осталось попыток: ${guessCount}`);
+        } else if (numberInput < secretNumber) {
+            alert(`Загаданное число больше, осталось попыток: ${guessCount}`);
+        }
+        asking(); // игра продолжается
+    }
 
 }
 
-//основная логика игры
+//запрашивает у пользователя ввод числа и проверяет его
 const asking = () => {
+    numberInput = prompt("Введи число от 1 до 100");
+
+    if (checkCancel(numberInput)) {
+        alert("Игра окончена");
+        return true;
+    }
+
+    if (!validateInput(numberInput)) {
+        asking();
+        return true;
+    }
+
+    numberInput = parseFloat(numberInput);
+
+    checkGuess(numberInput);
+}
+
+
+//основная логика игры
+const startGame = () => {
     title = confirm('Давай сыграем в игру "Загадывание случайного числа от 1 до 100"? Я загадываю, а ты должен угадать');
 
     // если была нажата кнопка "отмена" в первом prompt
@@ -68,8 +103,11 @@ const asking = () => {
     }
 
     secretNumber = randomNumber(1, 100);
-    console.log(secretNumber); //для проверка загаданного числа в конлсоли
+    console.log(secretNumber); //для проверки загаданного числа в консоли
 
-    guessNumber();
+    guessCount = 3;
+
+    asking();
 }
-asking();
+
+startGame();
